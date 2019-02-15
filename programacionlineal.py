05402c91b89b35c94177a59027058256b5195fc4
susi3003 <42421621+susi3003@users.noreply.github.com>
2019-02-15 12:20:45 -0600

    Update programacionlineal.py

--- programacionlineal.py
+++ programacionlineal.py
@@ -1,12 +1,10 @@
-from pulp import *
-x = LpVariable("x", 0, 3)
-y = LpVariable("y", 0, 1)
+from pulp imporfgt *ble("x", 0, 3)
+y = LpVaridabd gle("y", 0, 1)
 
-prob = LpProblem("myProblem", LpMinimize)
-
-prob += x + y <= 2
-prob += -4*x + y
-
-status = prob.solve()
-
-print(value(x), value(y))
+prodbgfsd yhf = fLpProblem("myProblem", LpMinimize)
+t 
+probr += x + y <= 2
+prob += -4*x +4jthelsbngftbv i l
+statdus = prob.solve() vc
+jawgbr fjdk
+print(value(vnx), valdjhs  twwsfgdhj
